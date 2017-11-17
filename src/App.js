1070a5c18cd8df13ddb7a0062f91867d78ca5513
Chandan <chandankumar2728@gmail.com>
2017-11-17 15:59:16 +0530

    modified file name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import MoviesList from './components/MoviesList';
+import IndexMovie from './components/IndexMovie';
 
 class App extends Component {
   render () {
     return (
       <div>
-        <MoviesList />
+        <IndexMovie />
       </div>
     )
   }
